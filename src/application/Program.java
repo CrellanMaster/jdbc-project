bfd4559b91b5832aa37145e67f9ff975a7b9a1c5
KodeMaster <devsandrounity@gmail.com>
2024-08-06 17:45:14 -0300

    Completed studies

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,37 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.exceptions.DbException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = DB.getConnection();
+
+			conn.setAutoCommit(false);
+
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+			conn.commit();
+			System.out.println("Rows1 : " + rows1 + " - Rows2 : " + rows2);
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new DbException("The transaction failed, a rollback was performed!");
+			} catch (SQLException e1) {
+				throw new DbException(e.getMessage());
+			}
+		}
 	}
 }
